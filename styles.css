8e702a11a67d01e63c94170f6613b9ba36e2157c
Halimhammadi <155792370+Halimhammadi@users.noreply.github.com>
2025-01-17 13:55:21 +0100

    styles.css

---
+++ styles.css
@@ -0,0 +1,10 @@
+
+function addToCart(productName) {
+    const messages = [
+        "تم إضافة " + productName + " إلى سلتك. هل أنت مستعد للمغامرات القادمة؟",
+        "احذر! " + productName + " قد يغير مجرى حياتك!",
+        "لقد قمت بإضافة " + productName + " إلى سلتك. لكن ماذا ستحصل عليه؟"
+    ];
+    const randomMessage = messages[Math.floor(Math.random() * messages.length)];
+    alert(randomMessage);
+}
